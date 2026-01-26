fa472d1cd1d57a01b7537fca8f8854ed96e6d90c
Angely <eusebioangely@gmail.com>
2026-01-26 04:01:12 -0500

    estrucutra hecha

---
+++ Submance.Infrastructure/Security/JwtTokenService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Submance.Infrastructure.Security
+{
+    public class JwtTokenService : ITokenService
+    {
+        public string GenerateToken(Usuario user) {  }
+    }
+}
